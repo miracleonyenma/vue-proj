97c7f854eb7e92c18acdd15674764e115e2205e4
miracleonyenma <miracleficient@gmail.com>
2020-03-16 10:58:36 +0100

    feat: Add bindings: v-bind, v-once, v-html

--- App_2/script.js
+++ App_2/script.js
@@ -1,11 +1,14 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Sup peeps!'
+        title: 'Sup peeps!',
+        link: '../app_1',
+        finishedLink: '<a href="../app_1">App_1</a>'
     },
     methods:{
         sayStuff: function (e){
-            return document.referrer
+            this.title = 'Sup'
+            return this.title
         }
     }
 })
