074b7c444b3e80f585606e90cbaca521dc4785ca
miracleonyenma <miracleficient@gmail.com>
2020-03-30 22:39:10 +0100

    update: dynamic styling(classes) using objects (js)
    - Using the computed property

--- App_4/script.js
+++ App_4/script.js
@@ -2,5 +2,13 @@ new Vue({
     el: "#app",
     data:{
         attachRed: false
+    },
+    computed:{
+        divClasses: function(){
+            return{
+                red: this.attachRed,
+                blue: !this.attachRed
+            }
+        }
     }
 })
