ebfadec43ec8f3e3f8b9dc7f8eec5844981ff00e
miracleonyenma <miracleficient@gmail.com>
2020-04-09 23:33:18 +0100

    update: looping through objects

--- app_6/script.js
+++ app_6/script.js
@@ -1,6 +1,12 @@
 new Vue({
     el: "#app",
     data: {
-        ingredients : ['maggi', 'okro', 'onions']
+        ingredients : ['maggi', 'okro', 'onions'],
+        persons: [
+            {name: "Ada", age: 24, color: "black"},
+            {name: "Chidi", age: 22, color: "white"},
+            {name: "Bola", age: 20, color: "yellow"}
+        ]
+
     }
 });
