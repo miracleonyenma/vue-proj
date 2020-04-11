c471d601508c2d689356f759f6fce6245a948be4
miracleonyenma <miracleficient@gmail.com>
2020-04-11 14:09:42 +0100

    update: minor changes to health bar, dynamic styling with :style directive
    - dynamic health bar colors

--- game_personal/js/script.js
+++ game_personal/js/script.js
@@ -1,6 +1,18 @@
 new Vue({
     el: "#app",
     data: {
-        show : false
+        started : false,
+        monster : {
+            health : 100,
+            attack : 5,
+            special : 10
+        },
+        player : {
+            health : 10,
+            attack : 3,
+            special : 7,
+            heal : 7
+        }
+
     }
 })
