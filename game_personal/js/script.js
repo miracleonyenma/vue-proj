e781e8f6706f7338f4a0012f42eb880004611703
miracleonyenma <miracleficient@gmail.com>
2020-04-12 22:16:05 +0100

    update && fix: generate attack values randomly, fix monster attack after reset bug

--- game_personal/js/script.js
+++ game_personal/js/script.js
@@ -11,46 +11,93 @@ new Vue({
         },
         player : {
             name: "player",
-            health : 10,
+            health : 45,
             attack : 3,
             special : 7,
-            heal : 7
+            heal : 8
         },
         logs : [
         ]
 
     },
     methods:{
+        randInt : function(min, max){
+            return Math.floor(Math.random(min - max) * max + min);
+        },
         Attack : function(mode, attacker, enemy){
-            if(!this.gameOver){
-                console.log(enemy[mode]);
-            
-                if(attacker.health > 0){
-                    enemy.health = enemy.health - attacker[mode];
-                    if(enemy.health <= 0){
-                        enemy.health = 0;
-                    }
-                    this.logs.unshift(this.addToLog(mode, attacker, enemy));
+            if(!this.gameOver && this.started && attacker.health > 0 && enemy.health > 0){     
+                console.log(attacker.name, attacker.health);
+                let attackval = this.randInt(attacker[mode], 12);
+                enemy.health = enemy.health - attackval;
+                this.logs.unshift(this.addToLog(attackval, attacker, enemy));
+                if(attacker == this.player){
+                    this.Attack("attack", enemy, attacker);
+                }
+                if(enemy.health <= 0 ){
+                    enemy.health = 0;
+                    confirm(attacker.name +" Won! \nPlay again?") ? this.resetAll() : null;
+                }
+                
+                // if(attacker.health > 0){
+                //     let attackval = this.randInt(attacker[mode], 12);
+                //     enemy.health = enemy.health - attackval;
+                //     if(enemy.health <= 0){
+                //         enemy.health = 0;
+                //     }
+                //     this.logs.unshift(this.addToLog(attackval, attacker, enemy));
     
-                    if(attacker == this.player){
-                        if(attacker.health <= 0){
-                            alert("you lost");
-                        }
-                        if(enemy.health <= 0){
-                            this.gameOver = true;
-                            confirm("You Won! \nPlay again?") ? this.resetAll() : null;
-                        }
-                        this.Attack("attack", enemy, attacker);
-                    }
-                }
+                //     if(attacker == this.player && enemy.health >= 0){
+                //         if(attacker.health <= 0 && enemy.health > 0){
+                //             this.gameOver = true;
+                //             alert("you lost");
+                //         }else if(enemy.health <= 0 && attacker.health > 0){
+                //             this.gameOver = true;
+                //             confirm("You Won! \nPlay again?") ? this.resetAll() : null;
+                //         }
+                //         this.Attack("attack", enemy, attacker);
+                //     }
+                // }
 
-                if(attacker == this.player && attacker.health <= 0){
-                    this.gameOver = true;
-                    confirm("You lost! \nPlay again?") ? this.resetAll() : null;
-                }
+                // if(attacker == this.player && attacker.health <= 0){
+                //     this.gameOver = true;
+                //     confirm("You lost! \nPlay again?") ? this.resetAll() : null;
+                // }
     
             }
+            
         },
+        // Attack : function(mode, attacker, enemy){
+        //     if(!this.gameOver && this.started && attacker.health > 0){     
+        //         console.log(attacker.name, attacker.health);
+                       
+        //         if(attacker.health > 0){
+        //             let attackval = this.randInt(attacker[mode], 12);
+        //             enemy.health = enemy.health - attackval;
+        //             if(enemy.health <= 0){
+        //                 enemy.health = 0;
+        //             }
+        //             this.logs.unshift(this.addToLog(attackval, attacker, enemy));
+    
+        //             if(attacker == this.player && enemy.health >= 0){
+        //                 if(attacker.health <= 0 && enemy.health > 0){
+        //                     this.gameOver = true;
+        //                     alert("you lost");
+        //                 }else if(enemy.health <= 0 && attacker.health > 0){
+        //                     this.gameOver = true;
+        //                     confirm("You Won! \nPlay again?") ? this.resetAll() : null;
+        //                 }
+        //                 this.Attack("attack", enemy, attacker);
+        //             }
+        //         }
+
+        //         if(attacker == this.player && attacker.health <= 0){
+        //             this.gameOver = true;
+        //             confirm("You lost! \nPlay again?") ? this.resetAll() : null;
+        //         }
+    
+        //     }
+            
+        // },
         heal : function(){
             if(!this.gameOver){
                 if(this.player.health <= 100){              
@@ -64,6 +111,7 @@ new Vue({
             }
         },
         resetAll : function(){
+            console.log("reset");
             this.started = true;
             this.player.health = 100;
             this.monster.health = 100;
@@ -72,13 +120,9 @@ new Vue({
         },
         addToLog : function(mode, attacker, enemy){
             if(mode === "heal"){
-                // return [attacker.name, attacker[mode]]
-                // return attacker.name + " heals for " + attacker[mode];
                 return {attacker: attacker.name, log: attacker.name + " heals for " + attacker[mode]} 
             }else{
-                // return [attacker.name, enemy.name, attacker[mode]]
-                // return attacker.name + " hits " + enemy.name + " for " + attacker[mode];
-                return {attacker: attacker.name, log: attacker.name + " hits " + enemy.name + " for " + attacker[mode]}; 
+                return {attacker: attacker.name, log: attacker.name + " hits " + enemy.name + " for " + mode}; 
             }            
         }
     }
